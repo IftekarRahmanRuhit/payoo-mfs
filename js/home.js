32fc7675f5280fdcce2ad5e95118bfd61e4815d3
IftekarRahmanRuhit <iftekarrahmanruhit2003@gmail.com>
2024-09-18 23:30:17 +0600

    added add money form

---
+++ js/home.js
@@ -0,0 +1,8 @@
+// step-01 add an event handler to the add money button inside the form
+document.getElementById('btn-add-money').addEventListener('click',function(event){
+    event.preventDefault();
+      
+    const addMoneyInput = document.getElementById('input-add-money').value;
+    const pinNumberInput = document.getElementById('input-pin').value;
+    console.log(addMoneyInput,pinNumberInput);
+})
