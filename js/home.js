42e8534dae659e8fc823b5c2fbecdd8616537906
IftekarRahmanRuhit <iftekarrahmanruhit2003@gmail.com>
2024-09-19 20:44:56 +0600

    upadated add money section

--- js/home.js
+++ js/home.js
@@ -1,8 +1,30 @@
 // step-01 add an event handler to the add money button inside the form
 document.getElementById('btn-add-money').addEventListener('click',function(event){
     event.preventDefault();
-      
+
     const addMoneyInput = document.getElementById('input-add-money').value;
     const pinNumberInput = document.getElementById('input-pin').value;
     console.log(addMoneyInput,pinNumberInput);
+    console.log('output');
+
+    if(pinNumberInput === '1234'){
+
+        const balance = document.getElementById('account-balance').innerText
+        console.log(balance);
+        const addMoney = parseFloat(addMoneyInput);
+        const balanceNumber = parseFloat(balance);
+        console.log(addMoney,balanceNumber);
+        const newBalance = addMoney + balanceNumber;
+        console.log(newBalance);
+        document.getElementById('account-balance').innerText = newBalance;
+
+    }
+    else{
+        alert('Failed to add money! Please try again');
+    }
+
+
+
+
+
 })
